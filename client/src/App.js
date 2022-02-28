3cadd6f3166373bac3af9d42226118d5a5ccbe63
maxh1231 <maxhumpherys@gmail.com>
2022-02-28 11:23:15 -0700

    apollo provider setup and Home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,49 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  ApolloLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { createUploadLink } from 'apollo-upload-client';
 import logo from './logo.svg';
 import './App.css';
 
+import Home from './Pages/Home'
+
+const uploadLink = createUploadLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: ApolloLink.from([authLink, uploadLink]),
+  cache: new InMemoryCache({
+
+  }),
+});
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </ApolloProvider>
   );
 }
 
---
+++ client/src/Pages/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <section>
+            <h1>Test</h1>
+        </section>
+    )
+}
+
+export default Home;
