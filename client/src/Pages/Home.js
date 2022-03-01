3ef930a4db84d7bf0f531eb27975dca473deea08
Max Humpherys <maxhumpherys@gmail.com>
2022-03-01 10:20:34 -0700

    basic home page styles

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -2,12 +2,12 @@ import hangingFlowers from '../assets/hanging-flowers.JPG';
 
 const Home = () => {
     return (
-        <section>
+        <section className='flex flex-col justify-center items-center h-screen'>
             <div>
                 <h2>Welcome to the Charred Rose Apothecary</h2>
             </div>
-            <div>
-                <img src={hangingFlowers} alt="Photo of hanging flowers"></img>
+            <div className=''>
+                <img src={hangingFlowers} alt="Photo of hanging flowers" className='h-[350px] w-[850px] object-cover'></img>
             </div>
             <div>
                 <span>All products are hand made, vegan, and cruelty free</span>
