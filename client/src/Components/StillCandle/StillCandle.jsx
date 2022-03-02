f282760e606254a507fccf9cd06a3c0e348499d6
Max Humpherys <maxhumpherys@gmail.com>
2022-03-02 14:08:20 -0700

    Still candle basic styles

--- client/src/Components/StillCandle/StillCandle.jsx
+++ client/src/Components/StillCandle/StillCandle.jsx
@@ -2,22 +2,22 @@ import stillCandle from '../../assets/still-candle.JPG'
 
 const StillCandle = () => {
     return (
-        <div>
+        <div className="flex flex-row justify-center text-white text-center">
             <div>
-                <img src={stillCandle} alt="Higher Candle"></img>
+                <img src={stillCandle} alt="Higher Candle" className="w-[350px] h-[350px] rounded-full object-cover"></img>
             </div>
             <div>
-                <h3>Still</h3>
-            </div>
-            <div>
-                <span>Lavender Chamomile</span>
-            </div>
-            <div>
-                <p><span>Agate</span> to sooth the nerves and spirit</p>
-                <p><span>Amethyst</span> to stabilize, add peace, balance, rids of negative energy</p>
-                <p><span>Lavender</span> for peace, calming, healing</p>
-                <p><span>Chamomile</span> to ease stress and anxiety, quiets the mind</p>
-                <p><span>Fluorite</span> to heighten intuitize insights, moves energy, absorbs negative energy and self doubt</p>
+                <div>
+                    <h3>Still</h3>
+                    <span>Lavender Chamomile</span>
+                </div>
+                <div>
+                    <p><span className="font-bold">Agate</span> to sooth the nerves and spirit</p>
+                    <p><span className="font-bold">Amethyst</span> to stabilize, add peace, balance, rids of negative energy</p>
+                    <p><span className="font-bold">Lavender</span> for peace, calming, healing</p>
+                    <p><span className="font-bold">Chamomile</span> to ease stress and anxiety, quiets the mind</p>
+                    <p><span className="font-bold">Fluorite</span> to heighten intuitize insights, moves energy, absorbs negative energy and self doubt</p>
+                </div>
             </div>
         </div>
     )
