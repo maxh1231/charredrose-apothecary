c27bebdc65f63975ab1b1c84828aa07ef2d9273d
Max Humpherys <maxhumpherys@gmail.com>
2022-03-02 19:29:28 -0700

    more contact styles

--- client/src/Pages/Contact.js
+++ client/src/Pages/Contact.js
@@ -1,22 +1,25 @@
 const Contact = () => {
     return (
-        <section className="h-screen text-white flex flex-col items-center justify-evenly">
+        <section className="h-screen text-white flex flex-col items-center ">
             <div>
-                <h2 className="text-center text-4xl underline underline-offset-8">
+                <h2 className="text-center text-4xl underline underline-offset-8 my-12 ">
                     Contact
                 </h2>
-                <h3 className="text-center">
+                <h3 className="text-center my-12">
                     Send us reviews or products you'd like to see!
                 </h3>
             </div>
-            <div>
+            <div className="flex flex-col items-center ">
                 <div>
-                    <input type='text' placeholder="Name" className="w-[250px] h-[40px] text-lg text-black"></input>
+                    <input type='text' placeholder="Name" className="my-4 w-[400px] h-[40px] text-lg text-black"></input>
                 </div>
                 <div>
-                    <textarea placeholder="Message">
+                    <textarea placeholder="Message" className="my-4 w-[400px] h-[100px] text-lg text-black">
 
                     </textarea>
+                </div>
+                <div>
+                    <button className="text-xl border-2 py-2 px-6">Submit</button>
                 </div>
             </div>
         </section>
